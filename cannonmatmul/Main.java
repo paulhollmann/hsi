ddde09ebe3dfa3f77de856810e29079a2882dc9a
Niklas <31795545+Bug-Patrick@users.noreply.github.com>
2023-12-10 18:37:01 +0100

    zyklische Vertauschung zur Initialisierung?

---
+++ cannonmatmul/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        var n = 3;
+        var A = new float[n*n];
+        var B = new float[n*n];
+        var C = new float[n*n];
+
+        var D = getCannonIteration(A, B, C);
+        for(int i = 0; i < D.length; i++)
+        {
+            System.out.println();
+        }
+
+    }
+
+    public static float[] getCannonIteration(float [] matA, float[] matB, float[] matC){
+        assert matA.length == matB.length && matA.length == matC.length;
+        var n = matA.length;
+
+
+        //Initiale steigernde zyklische Vertauschung
+        for (int i = 0; i < n; i++) { //rows (for A)
+            for (int j = 0; j < i; j++) { // amount of commutations
+                var firstAColumnElem = matA[i * n];
+                var firstBRowElem = matB[i];
+                for(int k = 0; k < n - 1; k++) { //column elements (for A)
+                    matA[i * n + k] = matA[i * n + k + 1];
+                    matB[k * n + i] = matB[(k+1) * n + i];
+                }
+                matA[i * n + n-1] = firstAColumnElem;
+                matB[(n-1) * n + i] = firstBRowElem;
+            }
+        }
+
+        
+        return matC;
+    }
+
+}
