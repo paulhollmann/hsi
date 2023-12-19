7e86da4fd9ffdab53590da326cdf689fb287ca9a
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-12-19 10:42:45 +0100

    Update build.sh

--- matmat/build.sh
+++ matmat/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -R ./lib/mpj-v0_44
+rm -R ./target
+
 ml java
 
 ./mvn.sh install:install-file -Dfile=lib/mpj-0.44.jar -DgroupId=mpj -DartifactId=mpj -Dversion=0.44 -Dpackaging=jar
@@ -9,3 +12,10 @@ ml java
 ./mvn.sh assembly:single
 
  tar -xf ./lib/mpj-v0_44.tar.gz --directory ./lib
+
+ mkdir out
+
+ mv target/matmat-0.0.1-SNAPSHOT-jar-with-dependencies.jar out/matmat.jar
+
+ rm -R ./target
+
