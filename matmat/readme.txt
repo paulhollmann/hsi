2c44784893f610c4ae09e4cf93f4a47f6139f530
Niklas <31795545+Bug-Patrick@users.noreply.github.com>
2023-12-21 15:18:50 +0100

    .

--- matmat/readme.txt
+++ matmat/readme.txt
@@ -1,6 +1,7 @@
 # to clone do
 
-git reset --hard && git pull && chmod +x build.sh run.sh mvn.sh
+git reset --hard && git pull && chmod +x build.sh run_ph.sh mvn.sh
+git reset --hard && git pull && chmod +x build.sh run_nb.sh mvn.sh
 
 
 # to build do
@@ -14,6 +15,7 @@ ml java
 ml intel
 
 export MPJ_HOME=/work/home/kurse/kurs00069/ph84wuqa/hsi/matmat/lib/mpj-v0_44
+export MPJ_HOME=/work/home/kurse/kurs00069/nb62hoty/hsi/matmat/lib/mpj-v0_44
 export PATH=$MPJ_HOME/bin:$PATH
 
 mpjrun.sh -np 4  out/matmat.jar
--- matmat/run_nb.sh
+++ matmat/run_nb.sh
@@ -5,15 +5,15 @@
 #SBATCH --reservation=kurs00069
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=niklas.beck@stud.tu-darmstadt.de
-#SBATCH -e /home/kurse/kurs00069/????????/hsi/matmat/logs/err.%j
-#SBATCH -o /home/kurse/kurs00069/???????/hsi/matmat/logs/out.%j
+#SBATCH -e /home/kurse/kurs00069/nb62hoty/hsi/matmat/logs/err.%j
+#SBATCH -o /home/kurse/kurs00069/nb62hoty/hsi/matmat/logs/out.%j
 
 #SBATCH --mem-per-cpu=3600
 #SBATCH --time=00:15:00
 #SBATCH -n 1
 #SBATCH -c 32
 
-cd /work/home/kurse/kurs00069/?????????/hsi/matmat/ || exit
+cd /work/home/kurse/kurs00069/nb62hoty/hsi/matmat/ || exit
 
 ./build.sh
 
@@ -21,7 +21,7 @@ module load intel
 module load java
 
 
-export MPJ_HOME=/work/home/kurse/kurs00069/?????????/hsi/matmat/lib/mpj-v0_44
+export MPJ_HOME=/work/home/kurse/kurs00069/nb62hoty/hsi/matmat/lib/mpj-v0_44
 export PATH=$MPJ_HOME/bin:$PATH
 
 srun mpjrun.sh -np 4  out/matmat.jar
