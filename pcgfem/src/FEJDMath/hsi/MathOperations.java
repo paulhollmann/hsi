ca3a5e84e0327b30b195a24a84201723fcee43ae
Niklas <31795545+Bug-Patrick@users.noreply.github.com>
2024-01-18 12:31:40 +0100

    s

--- pcgfem/src/FEJDMath/hsi/MathOperations.java
+++ pcgfem/src/FEJDMath/hsi/MathOperations.java
@@ -3,6 +3,44 @@ package FEJDMath.hsi;
 import java.util.Arrays;
 
 public class MathOperations {
+    public static double getDotProd(double[] vec1, double[] vec2){
+        assert vec1.length == vec2.length;
+        double result = 0.;
+        for (int i = 0; i < vec1.length; i++) {
+            result += vec1[i] * vec2[i];
+        }
+        return result;
+    }
+
+    public static double[] getInvJacobiPreconditioner(double[][] mat){
+        assert mat.length == mat[0].length;
+        var result = new double[mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            result[i] = 1.0/mat[i][i];
+        }
+        return result;
+    }
+
+    public static double[] getVectorVectorProduct(double[] vec1, double[] vec2){ // special
+        assert vec1.length == vec2.length ;
+        for (int i = 0; i < vec1.length; i++) {
+            vec1[i] *= vec2[i];
+        }
+        return vec1;
+    }
+
+    public static double[] getMatVecProd(double[][] mat, double[] vec){
+        assert mat[0].length == vec.length; // mat[0] or mat .length ??
+        var result = new double[mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < vec.length; j++) {
+                result[i]+= mat[i][j] * vec[j];
+            }
+        }
+        return result;
+    }
+
+
 
 
     public static void print(double[] v) {
--- pcgfem/src/FEJDMath/hsi/PCGSolver.java
+++ pcgfem/src/FEJDMath/hsi/PCGSolver.java
@@ -1,5 +1,7 @@
 package FEJDMath.hsi;
 
+import static FEJDMath.hsi.MathOperations.*;
+
 public class PCGSolver {
     /**
      * Solve double [ ].
@@ -9,8 +11,31 @@ public class PCGSolver {
      * @return Vektor v
      */
     public static double[] solve(double[][] K, double[] f) {
-        throw new NotImplementedException(); //todo Hausuebung 3
-    }
 
+        double[] v0 = new double[f.length];
+
+        double[] r0 = new double[f.length];
+        for(int i = 0; i < f.length; i++)
+        {
+            double[] temp = getMatVecProd(K, f);
+            r0[i] = f[i] - temp[0];
+        }
+
+        var C_inv  = getInvJacobiPreconditioner(K);
+        var d0 = getVectorVectorProduct(C_inv,r0);
+
+        var gamma0 = getDotProd(d0, r0);
+
+        // Vektor aus gammas erstellen?
+        var gamma_k = 0;
+        for(int k = 1; gamma_k < 0.0000001; k++)
+        {
+
+
+        }
+
+
+        return v0;
+    }
 
 }
