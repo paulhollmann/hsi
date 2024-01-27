9d506fc30507252df2b242a214ba15aacb74e127
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2024-01-27 15:40:10 +0100

    .

--- pcgfem/build.sh
+++ pcgfem/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#rm -R ./lib/mpj-v0_44
+rm -R ./lib/mpj-v0_44
 
 #ml java
 
-#./mvn.sh install:install-file -Dfile=lib/mpj-0.44.jar -DgroupId=mpj -DartifactId=mpj -Dversion=0.44 -Dpackaging=jar
+./mvn.sh install:install-file -Dfile=lib/mpj-0.44.jar -DgroupId=mpj -DartifactId=mpj -Dversion=0.44 -Dpackaging=jar
 
 ./mvn.sh
 
 ./mvn.sh assembly:single
 
-#tar -xf ./lib/mpj-v0_44.tar.gz --directory ./lib
+tar -xf ./lib/mpj-v0_44.tar.gz --directory ./lib
 
 mkdir out
 
